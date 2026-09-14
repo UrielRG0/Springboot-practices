925541224fd6454994aafd9a66288fbcf87d6964
UrielRG0 <urielrogu.16@gmail.com>
2026-09-14 01:30:58 -0600

    .

--- tacocloud/tacocloud-kitchen/src/main/java/tacos/Ingredient.java
+++ tacocloud/tacocloud-kitchen/src/main/java/tacos/Ingredient.java
@@ -8,11 +8,6 @@ public class Ingredient {
   private final String name;
   private final Type type;
 
-  public Ingredient(String name, Type type) {
-      this.name = name;
-      this.type = type;
-  }
-
   public enum Type {
     WRAP, PROTEIN, VEGGIES, CHEESE, SAUCE
   }
